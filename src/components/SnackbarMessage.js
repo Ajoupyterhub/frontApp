271279cba6474c9014e0fc567e883bb7960acff3
Jae Young Choi <mobilechoi@ajou.ac.kr>
2023-05-04 22:20:23 +0900

    Customized Snackbar recovered. apply mui style migration

--- src/components/SnackbarMessage.js
+++ src/components/SnackbarMessage.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { Snackbar } from '@mui/material';
-import MuiAlert from '@mui/material/Alert';
+import { IconButton, Snackbar, SnackbarContent } from '@mui/material';
+import {styled} from '@mui/material/styles';
+import { CheckCircle, Error, Info, Close, Warning } from '@mui/icons-material';
+import { amber, green } from '@mui/material/colors';
 
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+const variantIcon = {
+  success: CheckCircle,
+  warning: Warning,
+  error: Error,
+  info: Info,
+};
+
+const styles = {
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: 'error.dark', //theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: 'primary.main', //theme.palette.primary.main,
+  },
+  warning: {
+    backgroundColor: amber[700],
+  },
+  icon: {
+    fontSize: 20,
+    margin: 1,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: 1, //theme.spacing(1),
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: `1.5rem`,
+  },
+};
+
+const Span = styled('span')({
+  display : 'flex',
+  alignItems: 'center',
 });
 
-export default function SnackbarMessage(props) {
+function SnackbarMessage({ open, onClose, variant, message }) {
+  const classes = styles;
+  const Icon = variantIcon[variant];
   return (
     <div>
       <Snackbar
@@ -14,17 +54,33 @@ export default function SnackbarMessage(props) {
           vertical: 'bottom',
           horizontal: 'right',
         }}
-        open={props.open}
+        open={open}
         autoHideDuration={3000}
-        onClose={props.onClose}
+        onClose={onClose}
       >
-        <Alert
-          onClose={props.onClose}
-          severity={props.variant} 
-          sx={{width: '100%'}} >
-            {props.message}
-        </Alert>     
+        <SnackbarContent
+          sx={classes[variant]}
+          aria-describedby="client-snackbar"
+          message={
+            <Span id="client-snackbar" /*sx={classes.message}*/>
+              <Icon sx={classes.iconVariant} />
+              {message}
+            </Span>
+          }
+          action={[
+            <IconButton
+              key="close"
+              aria-label="close"
+              color="inherit"
+              onClick={onClose}
+            >
+              <Close sx={classes.icon} />
+            </IconButton>,
+          ]}
+        />
       </Snackbar>
     </div>
   );
 }
+
+export default SnackbarMessage;
